39aec6add8d59064734457b05c683a337f6c3722
constancacc <constancamfcorreia@gmail.com>
2024-06-01 11:34:32 +0100

    bug fix — botões index

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var labels = document.querySelectorAll('label.custom-radio');
+    var labels = document.querySelectorAll('label.contacts');
+    var label = document
+
     labels.forEach(function(label) {
         var link = label.querySelector('p');
         link.addEventListener('click', function() {
             window.location.href = "contacts.html"; // Redireciona para a página desejada
         });
+    
+
     });
 });
