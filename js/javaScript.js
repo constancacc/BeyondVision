46efae0a39e64fefaea94e8d3d5a3a5aa4e5942d
constancacc <constancamfcorreia@gmail.com>
2024-06-04 17:37:36 +0100

    página das notificações

--- js/javaScript.js
+++ js/javaScript.js
@@ -1,15 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var labels = document.querySelectorAll('label.contacts');
-    var contact = document.querySelector('form');
 
     // ir para outra página só a selecionar uma opção
     labels.forEach(function(label) {
         var link = label.querySelector('p');
         link.addEventListener('click', function() {
-            window.location.href = "contacts.html"; // Redireciona para a página desejada
+            window.location.href = "./contacts.html"; // Redireciona para a página desejada
         });
     });
-
-    contact.submit();//submeter formulário sem botão
     
 });
