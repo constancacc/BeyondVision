1f12b24505c26d3252eb50c90d19b9b9856d2ac3
constancacc <constancamfcorreia@gmail.com>
2024-06-05 15:06:23 +0100

    localizações

--- js/javaScript.js
+++ js/javaScript.js
@@ -10,3 +10,38 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+function initMap() {
+    // Tenta obter a localização atual do usuário
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var lat = position.coords.latitude;
+            var lng = position.coords.longitude;
+            var iframe = document.getElementById('gmap_canvas');
+            iframe.src = `https://maps.google.com/maps?q=${lat},${lng}&z=13&ie=UTF8&iwloc=&output=embed`;
+        }, function() {
+            handleLocationError(true);
+        });
+    } else {
+        // Browser não suporta Geolocation
+        handleLocationError(false);
+    }
+}
+
+function handleLocationError(browserHasGeolocation) {
+    console.log(browserHasGeolocation ?
+        'Erro: O serviço de Geolocalização falhou.' :
+        'Erro: Seu navegador não suporta geolocalização.');
+}
+
+// Função para mudar a localização do mapa
+function changeLocation(address) {
+    var location_title = document.querySelector('h4.location_title');
+    location_title.style.display = "none";
+    
+    var iframe = document.getElementById('gmap_canvas');
+    iframe.src = `https://maps.google.com/maps?q=${encodeURIComponent(address)}&z=13&ie=UTF8&iwloc=&output=embed`;
+}
+
+// Inicializa o mapa assim que a página é carregada
+window.onload = initMap;
