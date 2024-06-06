5ec34808e59e904c98c353f413f06ddd9cffdc34
constancacc <constancamfcorreia@gmail.com>
2024-06-06 12:36:25 +0100

    localização notificação

--- js/javaScript.js
+++ js/javaScript.js
@@ -9,39 +9,21 @@ document.addEventListener("DOMContentLoaded", function() {
             window.location.href = "./html/contacts.html";
         });
     });
+
+    //Localização da Notificação
+    var infoElement = document.querySelector('p.info');
+    
+   
+    if (infoElement) {
+        var address = infoElement.textContent;
+        updateLocation(address);
+    }
 });
 
-/*IFRAME COM LOCALIZAÇÃO VARIÁVEL*/
-function initMap() {
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var lat = position.coords.latitude;
-            var lng = position.coords.longitude;
-            var iframe = document.getElementById('gmap_canvas');
-            iframe.src = `https://maps.google.com/maps?q=${lat},${lng}&z=13&ie=UTF8&iwloc=&output=embed`;
-        }, function() {
-            handleLocationError(true);
-        });
-    } else { 
-        handleLocationError(false);
-    }
-}
-
-function handleLocationError(browserHasGeolocation) {
-    console.log(browserHasGeolocation ?
-        'Erro: O serviço de Geolocalização falhou.' :
-        'Erro: Seu navegador não suporta geolocalização.');
-}
-
-// Mostrar a localização da queda
-function changeLocation(address) {
-    var location_title = document.querySelector('h4.location_title');
-    location_title.style.display = "none";
-
+// Função para atualizar a localização do iframe
+function updateLocation(address) {
     var iframe = document.getElementById('gmap_canvas');
     iframe.src = `https://maps.google.com/maps?q=${encodeURIComponent(address)}&z=13&ie=UTF8&iwloc=&output=embed`;
 }
 
-// Inicializa o mapa assim que a página é carregada
-window.onload = initMap;
